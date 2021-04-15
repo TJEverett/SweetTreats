20e0e5abb2adfd21b264ae5d7fd571cdd04b32b7
Tristen Everett <spock214@gmail.com>
2021-04-15 11:48:18 -0700

    add ability to interact with database

--- SweetTreats/Models/SweetTreatsContext.cs
+++ SweetTreats/Models/SweetTreatsContext.cs
@@ -5,7 +5,9 @@ namespace SweetTreats.Models
 {
   public class SweetTreatsContext : IdentityDbContext<ApplicationUser>
   {
-
+    public virtual DbSet<Treat> Treats { get; set; }
+    public virtual DbSet<Flavor> Flavors { get; set; }
+    public virtual DbSet<FlavorTreat> FlavorTreat { get; set; }
 
     public SweetTreatsContext(DbContextOptions options) : base(options) { }
   }
