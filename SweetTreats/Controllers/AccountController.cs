0f26c53a2630d2e3315697bdf5caaa23f58e938b
Tristen Everett <spock214@gmail.com>
2021-04-21 11:53:31 -0700

    add a register view to the account controller

--- SweetTreats/Controllers/AccountController.cs
+++ SweetTreats/Controllers/AccountController.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using SweetTreats.Models;
-// using SweetTreats.ViewModels;
+using SweetTreats.ViewModels;
 using System.Threading.Tasks;
 
 namespace SweetTreats.Controllers
@@ -22,6 +22,30 @@ namespace SweetTreats.Controllers
     public ActionResult Index()
     {
       return View();
+    }
+
+    public ActionResult Register()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> Register(RegisterViewModel model)
+    {
+      if(model.ConfirmPassword != model.Password)
+      {
+        return RedirectToAction("Register");
+      }
+      ApplicationUser user = new ApplicationUser { UserName = model.Email };
+      IdentityResult result = await _userManager.CreateAsync(user, model.Password);
+      if(result.Succeeded)
+      {
+        return RedirectToAction("Index");
+      }
+      else
+      {
+        return View();
+      }
     }
   }
 }
