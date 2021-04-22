080758e440113fd69b617533ce5378463a4e639a
Tristen Everett <spock214@gmail.com>
2021-04-22 11:15:48 -0700

    fix password field contents being shown when creating an account

--- SweetTreats/ViewModels/RegisterViewModel.cs
+++ SweetTreats/ViewModels/RegisterViewModel.cs
@@ -15,7 +15,7 @@ namespace SweetTreats.ViewModels
     public string Password { get; set; }
 
     [DataType(DataType.Password)]
-    [Display(Name = "Confirm password")]
+    [Display(Name = "Confirm Password")]
     [Compare("Password", ErrorMessage = "The password and confirmation password do not match")]
     public string ConfirmPassword { get; set; }
   }
