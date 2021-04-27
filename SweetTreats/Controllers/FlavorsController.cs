eaa15089b64f2a4aaeabef1b599ee924bd686dee
Tristen Everett <spock214@gmail.com>
2021-04-27 11:56:08 -0700

    add details view and functionality to controller

--- SweetTreats/Controllers/FlavorsController.cs
+++ SweetTreats/Controllers/FlavorsController.cs
@@ -41,5 +41,15 @@ namespace SweetTreats.Controllers
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
+
+    [AllowAnonymous]
+    public ActionResult Details(int id)
+    {
+      Flavor thisFlavor = _db.Flavors
+        .Include(flavor => flavor.Treats)
+          .ThenInclude(join => join.Treat)
+        .FirstOrDefault(flavor => flavor.FlavorId == id);
+      return View(thisFlavor);
+    }
   }
 }
